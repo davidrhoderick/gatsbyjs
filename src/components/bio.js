02f1a23cdf9d0ee8fd0c9119d6a07eb4986fe1c0
David Rhoderick <david@vrio.com>
2023-08-12 13:12:28 -0400

    Installing and configuring ESLint and Prettier

--- src/components/bio.js
+++ src/components/bio.js
@@ -14,7 +14,6 @@ const Bio = () => {
       # if there was more than one user, this would need to be filtered
       author: wpUser {
         firstName
-        twitter: name
         description
         avatar {
           url
@@ -34,19 +33,12 @@ const Bio = () => {
           src={avatarUrl}
         />
       )}
-      {author?.firstName && (
+      (
         <p>
-          Written by <strong>{author.firstName}</strong>
-          {` `}
           {author?.description || null}
-          {` `}
-          {author?.twitter && (
-            <a href={`https://twitter.com/${author?.twitter || ``}`}>
-              You should follow them on Twitter
-            </a>
-          )}
         </p>
-      )}
+          
+      )
     </div>
   )
 }
