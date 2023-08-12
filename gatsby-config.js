9aae6cbf94cc77d66a26924f802639a25c9dfa1e
David Rhoderick <david@vrio.com>
2023-08-12 13:11:56 -0400

    Installing Google Tag Manager Gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,6 +93,24 @@ module.exports = {
       },
     },
 
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-P5M7K3B4",
+  
+        // Include GTM in development.
+        //
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: true,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        //
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" },
+      },
+    },
+
     /**
      * this (optional) plugin enables Progressive Web App + Offline functionality
      * To learn more, visit: https://gatsby.dev/offline
