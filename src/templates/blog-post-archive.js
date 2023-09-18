a2c61f04e0246fe98328158c97c166f193142f81
David Rhoderick <david@iskratech.rs>
2023-09-18 10:22:29 +0200

    Fixing deprecated GraphQL query for post archive.

--- src/templates/blog-post-archive.js
+++ src/templates/blog-post-archive.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import parse from "html-react-parser"
+import React from 'react'
+import { Link, graphql } from 'gatsby'
+import parse from 'html-react-parser'
 
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Bio from '../components/bio'
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const BlogIndex = ({
   data,
@@ -72,11 +72,7 @@ export default BlogIndex
 
 export const pageQuery = graphql`
   query WordPressPostArchive($offset: Int!, $postsPerPage: Int!) {
-    allWpPost(
-      sort: { fields: [date], order: DESC }
-      limit: $postsPerPage
-      skip: $offset
-    ) {
+    allWpPost(sort: { date: DESC }, limit: $postsPerPage, skip: $offset) {
       nodes {
         excerpt
         uri
